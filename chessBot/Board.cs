0ee98364eeb2b472580f631d074741323cdba99e
Eyrim <marfxedbot@gmail.com>
2021-03-30 00:06:23 +0100

    Laid down some JSON foundations, started writing some plans in Board.cs

---
+++ chessBot/Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace chessBot
+{
+    public class Board
+    {
+        public static string[][] currentBoard { get; set; }
+
+        public static bool WriteToFile(string filePath)
+        {
+            try
+            {
+                //TODO: Check the file path is valid
+                StreamWriter sw = new StreamWriter(filePath);
+                JsonSerializer js = new JsonSerializer();
+
+                // Chess boards have 64 tiles
+
+                /*
+                 * Map out the board in JSON, have each tile with it's own key - DONE
+                 * 
+                 * Parse the board into a 3d dict | board["h"]["2"] = "Q"
+                 * 
+                 * 
+                 */
+
+
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
--- chessBot/Program.cs
+++ chessBot/Program.cs
@@ -6,7 +6,7 @@ namespace chessBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
